a8da350a6826d81d09bc70246c971c60ec84e868
Suzuki Takeo <takeo@stko.info>
2020-03-29 12:12:10 +0900

    wip

--- dao/AccountCredentialsDao.go
+++ dao/AccountCredentialsDao.go
@@ -4,5 +4,10 @@ import "context"
 import model "github.com/BambooTuna/quest-market/model"
 
 type AccountCredentialsDao interface {
-	ResolveByAccountId(ctx context.Context, accountId string) model.AccountCredentials
+	ResolveByAccountId(ctx context.Context, accountId string) (*model.AccountCredentials, error)
+	ResolveByMail(ctx context.Context, mail string) (*model.AccountCredentials, error)
+	Insert(ctx context.Context, record *model.AccountCredentials) error
+	UpdateMail(ctx context.Context, newMail string) error
+	UpdatePassword(ctx context.Context, newPassword string) error
+	DeleteByAccountId(ctx context.Context, accountId string) error
 }
