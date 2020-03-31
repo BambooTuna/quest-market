056155a58f33fbc73478954dcba27dd56378fc74
Suzuki Takeo <takeo@stko.info>
2020-03-31 11:23:10 +0900

    priceの型を戻す

--- backend/json/ExhibitionRequestJson.go
+++ backend/json/ExhibitionRequestJson.go
@@ -2,22 +2,17 @@ package json
 
 import (
 	"github.com/BambooTuna/quest-market/backend/command"
-	"strconv"
 )
 
 type ExhibitionRequestJson struct {
 	Title  string `json:"title"`
 	Detail string `json:"detail"`
-	Price  string `json:"price"`
+	Price  int64  `json:"price"`
 	State  string `json:"state"`
 }
 
 func (e ExhibitionRequestJson) GenerateProductDetailCommand() command.ProductDetailCommand {
-	price, err := strconv.ParseInt(e.Price, 10, 64)
-	if err != nil {
-		price = 0
-	}
-	return command.ProductDetailCommand{Title: e.Title, Detail: e.Detail, Price: price, State: e.State}
+	return command.ProductDetailCommand{Title: e.Title, Detail: e.Detail, Price: e.Price, State: e.State}
 }
 
 func (e ExhibitionRequestJson) GenerateExhibitionCommand(presenterId string) command.ExhibitionCommand {
