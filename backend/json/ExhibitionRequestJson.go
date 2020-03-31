26807cdf4c5f262b97c559ab2802620d16bee393
Suzuki Takeo <takeo@stko.info>
2020-03-31 11:04:59 +0900

    jsonパースエラーに対する応急処置

--- backend/json/ExhibitionRequestJson.go
+++ backend/json/ExhibitionRequestJson.go
@@ -1,16 +1,23 @@
 package json
 
-import "github.com/BambooTuna/quest-market/backend/command"
+import (
+	"github.com/BambooTuna/quest-market/backend/command"
+	"strconv"
+)
 
 type ExhibitionRequestJson struct {
 	Title  string `json:"title"`
 	Detail string `json:"detail"`
-	Price  int64  `json:"price"`
+	Price  string `json:"price"`
 	State  string `json:"state"`
 }
 
 func (e ExhibitionRequestJson) GenerateProductDetailCommand() command.ProductDetailCommand {
-	return command.ProductDetailCommand{Title: e.Title, Detail: e.Detail, Price: e.Price, State: e.State}
+	price, err := strconv.ParseInt(e.Price, 10, 64)
+	if err != nil {
+		price = 0
+	}
+	return command.ProductDetailCommand{Title: e.Title, Detail: e.Detail, Price: price, State: e.State}
 }
 
 func (e ExhibitionRequestJson) GenerateExhibitionCommand(presenterId string) command.ExhibitionCommand {
