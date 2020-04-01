88cfbb7903314f36b623c938a785b0efbb6d563c
Suzuki Takeo <takeo@stko.info>
2020-04-01 23:43:41 +0900

    front: 残高view追加

---
+++ front/src/components/parts/BalanceWindow.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="balance-window">
+    <WaitLoading :loading_flag="loadingFlag">
+      <p class="price">残高: ¥ {{item.balance}}</p>
+    </WaitLoading>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+import { Balance } from '@/lib/RestAPIProtocol'
+import WaitLoading from '@/components/parts/WaitLoading.vue'
+
+@Component({
+  components: {
+    WaitLoading
+  }
+})
+export default class BalanceWindow extends Vue {
+    @Prop()
+    private item!: Balance;
+
+    @Prop()
+    private loadingFlag!: boolean
+}
+</script>
+
+<style scoped>
+  .balance-window {
+    padding: 0.5em 1em;
+    margin: 2em 0;
+    font-weight: bold;
+    color: #6091d3;/*文字色*/
+    background: #FFF;
+    border: solid 3px #6091d3;/*線*/
+    border-radius: 10px;/*角の丸み*/
+  }
+
+  .price {
+    color: #962f10;/*文字色*/
+    text-align: left;
+    border-radius: 0.5em;/*角丸*/
+  }
+</style>
--- front/src/views/MyPage.vue
+++ front/src/views/MyPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="my-page">
     <Authentication>
+      <BalanceWindow :item="item" :loadingFlag="loadingFlag"></BalanceWindow>
       <h2>自分の出品一覧</h2>
       <PrivateProductsTable :params="this.$route.query"></PrivateProductsTable>
     </Authentication>
@@ -11,13 +12,28 @@
 import { Component, Vue } from 'vue-property-decorator'
 import Authentication from '@/components/Authentication.vue'
 import PrivateProductsTable from '@/components/PrivateProductsTable.vue'
+import BalanceWindow from '@/components/parts/BalanceWindow.vue'
+import API from '@/lib/RestAPI'
+import { Balance } from '@/lib/RestAPIProtocol'
+
 @Component({
   components: {
-    Authentication, PrivateProductsTable
+    Authentication, PrivateProductsTable, BalanceWindow
   }
 })
 
 export default class MyPage extends Vue {
+    private api: API =new API()
+    private item!: Balance
+    private loadingFlag?: boolean = true
+
+    created (): void {
+      this.api.getBalance()
+        .then((balance: Balance) => {
+          this.item = balance
+          this.loadingFlag = false
+        })
+    }
 }
 </script>
 
